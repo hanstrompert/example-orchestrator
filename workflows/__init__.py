ec26499b5b6df37c22a4d82abc02b1a528f6c34d
Hans Trompert <hans.trompert@surf.nl>
2022-10-21 14:57:00 +0200

    terminate user workflow

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -5,3 +5,4 @@ LazyWorkflowInstance("workflows.user_group.modify_user_group", "modify_user_grou
 LazyWorkflowInstance("workflows.user_group.terminate_user_group", "terminate_user_group")
 LazyWorkflowInstance("workflows.user.create_user", "create_user")
 LazyWorkflowInstance("workflows.user.modify_user", "modify_user")
+LazyWorkflowInstance("workflows.user.terminate_user", "terminate_user")
