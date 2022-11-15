2f172f2c69db4c9249ab9f709438f3e1eaeb0c26
Hans Trompert <hans.trompert@surf.nl>
2022-11-15 16:05:34 +0100

    emphasize that choice_list returns a list of user group IDs

--- workflows/user/create_user.py
+++ workflows/user/create_user.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import uuid4
 
 from orchestrator.db.models import ProductTable, SubscriptionTable
@@ -45,7 +46,7 @@ def initial_input_form_generator(product_name: str) -> FormGenerator:
 
         username: str
         age: int | None
-        user_group_id: user_group_selector()  # type:ignore
+        user_group_ids: user_group_selector()  # type:ignore
 
     user_input = yield CreateUserForm
 
@@ -65,10 +66,10 @@ def create_subscription(product: UUIDstr) -> State:
 
 
 @step("Initialize subscription")
-def initialize_subscription(subscription: UserInactive, username: str, age: int, user_group_id: str) -> State:
+def initialize_subscription(subscription: UserInactive, username: str, age: int, user_group_ids: List[str]) -> State:
     subscription.user.username = username
     subscription.user.age = age
-    subscription.user.group = UserGroup.from_subscription(user_group_id[0]).user_group
+    subscription.user.group = UserGroup.from_subscription(user_group_ids[0]).user_group
     subscription.description = (
         f"User {username} from group {subscription.user.group.group_name} ({subscription.affiliation})"
     )
